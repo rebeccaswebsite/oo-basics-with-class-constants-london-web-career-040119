3e5099eb1b4f92d9730473b4fcc748db6ca99e3b
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-04-04 16:54:49 +0100

    add Book and Shoe class

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,15 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition, :brand
+
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    if BRANDS.include?(brand)
+      BRANDS
+    else
+    BRANDS << brand
+    end
   end
 
   def cobble
